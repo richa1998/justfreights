99dabac47ed37533d92a61f111eda3b6cc614f30
richa <richa@gmail.com>
2020-02-17 16:48:24 +0530

    added one file

---
+++ sub.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+
+<div class="header">
+  <p>You have not Deleted your account just yet....select an option below!</p>
+</div>
+
+<div class="content">
+
+  <div class="left">
+    &nbsp;&nbsp;&nbsp;200+ shipping<br> &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;Agents<br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;are using <br>JUST FREIGHTS<br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;daily<br>and growing their <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;business 3x
+</div>
+<div class="right">
+  <form>
+  <table>
+<tr><td><input type="radio" name="sub" value="features"><label>Missing features we need</label></td></tr>
+
+ <tr><td><input type="radio" name="sub" value="switch"><label>Switching to another</label></td></tr>
+
+<tr><td> <input type="radio" name="sub" value="expensive"><label>Too Expensive......</label></td></tr>
+
+ <tr><td>  <input type="radio" name="sub" value="shut"><label>Shutting down business</label></td></tr>
+
+  <tr><td> <input type="radio" name="sub" value="other"><label>Other reason.....</label></td></tr>
+
+</table>
+</form>
+  </div>
+  
+</div>
+
+</body>
+</html>
